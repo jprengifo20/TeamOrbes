4cb8bc827e8c4608f1bf415f97717776aea9e036
LuisDextre99 <Luis Angel@DESKTOP-STV49GH>
2019-10-22 10:54:32 -0500

    Se implemento la entidad incidencia

--- src/main/java/com/pw/model/entity/Incident.java
+++ src/main/java/com/pw/model/entity/Incident.java
@@ -1,34 +1,33 @@
- package com.pw.model.entity;
+package com.pw.model.entity;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.annotations.Type;
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
-@Table(name = "requests")
+@Table(name = "incidents")
 public class Incident {
+
 	@Id
+	@Column(name = "incident_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
-private String description;
-private String state;
-private String date;
-
-
-private double price_unit;
-	@ManyToMany(mappedBy = "categories")
-	private List<Book> books;
-
-	public Incident() {
-	}
 
 	public Long getId() {
 		return id;
@@ -38,14 +37,55 @@ private double price_unit;
 		this.id = id;
 	}
 
-	
-
-	public List<Book> getBooks() {
-		return books;
+	public String getGravity() {
+		return gravity;
 	}
 
-	public void setBooks(List<Book> books) {
-		this.books = books;
+	public void setGravity(String gravity) {
+		this.gravity = gravity;
 	}
 
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getObservations() {
+		return observations;
+	}
+
+	public void setObservations(String observations) {
+		this.observations = observations;
+	}
+
+	@NotEmpty
+	private String gravity;
+
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "incident_category", 
+				joinColumns = { @JoinColumn(name = "incident_id") }, 
+				inverseJoinColumns = {@JoinColumn(name = "category_id") 
+			})
+	private List<Category> categories = new ArrayList<>();
+
+	@Column(name = "date")
+	@DateTimeFormat(pattern = "yyyy")
+	private Date date;
+
+	@Lob
+	@NotEmpty
+	@Type(type = "org.hibernate.type.TextType")
+	private String observations;
 }
