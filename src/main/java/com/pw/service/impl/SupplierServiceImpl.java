101672c1d204954c53b1eff5886d0c5b77a5b13c
LuisDextre99 <Luis Angel@DESKTOP-STV49GH>
2019-10-22 16:30:51 -0500

    Se implemento SupplierServiceImpl

---
+++ src/main/java/com/pw/service/impl/SupplierServiceImpl.java
@@ -0,0 +1,53 @@
+package com.pw.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pw.model.entity.Supplier;
+import com.pw.model.repository.SupplierRepository;
+import com.pw.service.SupplierService;
+
+@Service
+public class SupplierServiceImpl implements SupplierService {
+
+	@Autowired
+	private SupplierRepository supplierRepository;
+	
+	@Override
+	public List<Supplier> getAll()
+	{
+		return supplierRepository.findAll();
+	}
+	
+	@Override
+	public Supplier getOneById(Long id)
+	{
+		return supplierRepository.findById(id).orElseThrow(()->new RuntimeException("Proveedor no encontrado!"));
+	}
+	
+	@Override
+	public Long create(Supplier entity)
+	{
+		supplierRepository.save(entity);
+		return entity.getId();
+	}
+	
+	@Override
+	public void update(Long id, Supplier entity)
+	{
+		Supplier currentSupplier=getOneById(id);
+		currentSupplier.setEnterprise(entity.getEnterprise());
+		currentSupplier.setRuc(entity.getRuc());
+		currentSupplier.setProduct(entity.getProduct());
+		currentSupplier.setPhone(entity.getPhone());
+		supplierRepository.save(currentSupplier);
+	}
+	
+	@Override
+	public void delete(Long id)
+	{
+		supplierRepository.deleteById(id);
+	}
+}
