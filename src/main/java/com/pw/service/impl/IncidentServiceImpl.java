d15464fbef6941443683dd97f774cf8a9b7df9a8
LuisDextre99 <Luis Angel@DESKTOP-STV49GH>
2019-10-22 11:15:52 -0500

    Se implentó la clase incidentServiceImpl

---
+++ src/main/java/com/pw/service/impl/IncidentServiceImpl.java
@@ -0,0 +1,55 @@
+package com.pw.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pw.model.entity.Incident;
+import com.pw.model.repository.IncidentRepository;
+import com.pw.service.IncidentService;
+
+@Service
+public class IncidentServiceImpl implements IncidentService {
+	
+	@Autowired
+	private IncidentRepository incidentRepository;
+
+	@Override
+	public List<Incident> getAll() {
+		// TODO Auto-generated method stub
+		return incidentRepository.findAll();
+	}
+
+	@Override
+	public Incident getOneById(Long id) {
+		// TODO Auto-generated method stub
+		return incidentRepository.findById(id).orElseThrow(() -> new RuntimeException("Incident not found"));
+	}
+
+	@Override
+	public Long create(Incident entity) {
+		// TODO Auto-generated method stub
+		incidentRepository.save(entity);
+		return entity.getId();
+	}
+
+	@Override
+	public void update(Long id, Incident entity) {
+		// TODO Auto-generated method stub
+		Incident currentIncident= getOneById(id);
+		currentIncident.setCategories(entity.getCategories());
+		currentIncident.setDate(entity.getDate());
+		currentIncident.setGravity(entity.getGravity());
+		currentIncident.setObservations(entity.getObservations());
+		incidentRepository.save(currentIncident);
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		incidentRepository.deleteById(id); 
+	} 
+	
+	
+}
