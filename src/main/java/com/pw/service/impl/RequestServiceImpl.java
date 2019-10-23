4ec29f80c8f1c71f9ce8c68c6a0f0a7c10d83949
jprengifo <jprengifo@10.11.148.62>
2019-10-22 22:47:45 -0500

    Se agrego el servicio implementado: RequestServiceImpl

---
+++ src/main/java/com/pw/service/impl/RequestServiceImpl.java
@@ -0,0 +1,59 @@
+package com.pw.service.impl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pw.model.entity.Request;
+import com.pw.model.repository.RequestRepository;
+import com.pw.service.RequestService;
+
+@Service
+public class RequestServiceImpl implements RequestService {
+
+	@Autowired
+	private RequestRepository requestRepository;
+
+	@Override
+	public List<Request> getAll() {
+		return requestRepository.findAll();
+	}
+
+	@Override
+	public Request getOneById(Long id) {
+		return requestRepository.findById(id).orElseThrow(() -> new RuntimeException("Request Not Found!"));
+	}
+
+	@Override
+	public Long create(Request entity) {
+		requestRepository.save(entity);
+        return entity.getId();
+	}
+
+	@Override
+	public void update(Long id, Request entity) {
+		Request currentRequest = getOneById(id);
+		currentRequest.setArea(entity.getArea());
+		currentRequest.setProduct(entity.getProduct());
+		currentRequest.setDescription(entity.getDescription());
+		currentRequest.setQuantity(entity.getQuantity());
+		currentRequest.setState(entity.getState());
+		currentRequest.setDate(entity.getDate());
+		currentRequest.setUnit_price(entity.getUnit_price());
+
+		requestRepository.save(currentRequest);
+	}
+
+	@Override
+	public void delete(Long id) {
+		requestRepository.deleteById(id);
+	}
+
+	
+	
+	
+	
+}
